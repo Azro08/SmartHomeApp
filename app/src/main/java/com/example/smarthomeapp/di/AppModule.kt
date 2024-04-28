cb2d74624adaf7eaea6d8b84009da48d15ad4bd7
azro08 <azro.skib08@gmail.com>
2024-04-29 01:12:22 +0300

    fix di

--- app/src/main/java/com/example/smarthomeapp/di/AppModule.kt
+++ app/src/main/java/com/example/smarthomeapp/di/AppModule.kt
@@ -1,22 +1,41 @@
 package com.example.smarthomeapp.di
 
 import android.content.Context
+import com.example.smarthomeapp.data.repository.AuthRepositoryImpl
+import com.example.smarthomeapp.data.repository.UsersRepositoryImpl
+import com.example.smarthomeapp.domain.repository.AuthRepository
+import com.example.smarthomeapp.domain.repository.UsersRepository
 import com.example.smarthomeapp.util.AuthManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
     @Provides
-    @Singleton
     fun provideAuthManager(@ApplicationContext context: Context): AuthManager =
         AuthManager(context)
 
+    @Provides
+    fun provideAuthRepository(
+        firebaseAuth: FirebaseAuth,
+        firebaseFirestore: FirebaseFirestore
+    ): AuthRepository = AuthRepositoryImpl(
+        firebaseAuth, firebaseFirestore
+    )
+
+    @Provides
+    fun provideUsersRepository(
+        firebaseAuth: FirebaseAuth,
+        firebaseFirestore: FirebaseFirestore
+    ): UsersRepository = UsersRepositoryImpl(
+        firebaseFirestore, firebaseAuth
+    )
 
 }
